78432cdcd9bf14885f140b0c25b153eda2d0f1cd
Paula <paulamazoster@gmail.com>
2020-04-16 21:23:38 -0500

    Dashboard codebase

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,16 @@ const exceptionMiddleware = require('./middleware/exception.middleware')
 const authenticationMiddleware = require('./middleware/authentication.middleware')
 const port = 8080
 
-app.use(cors())
+/* app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "POST, PUT, DELETE, GET, OPTIONS");
+    next();
+}); */
+
+app.use(cors({
+    origin: 'http://127.0.0.1:5500'
+  }));
 
 app.use(authenticationMiddleware.jwt)
 
---
+++ frontend/index.js
@@ -0,0 +1,53 @@
+const res = axios.create({
+    method: 'get',
+    baseURL: 'localhost:8080',
+    headers: { 'authorization': 'abc', 'Access-Control-Allow-Origin': '*'}
+});
+
+const getCoronaDatos = res.get('/statistic/co');
+getCoronaDatos.then( (r) => {
+    console.log(r);
+});
+
+const chartData = {
+    data: {
+        labels: ['2020-04-10', '2020-04-11', '2020-04-12', '2020-04-13', '2020-04-14', '2020-04-15'],
+        datasets: [{
+            label: 'Confirmados',
+            data: [2473, 2709, 2776, 2852, 2979, 3105],
+            backgroundColor: 'rgba(63, 121, 191, 0.2)',
+            borderColor: 'rgba(63, 121, 191, 1)',
+            borderWidth: 1
+        },{
+            label: 'Muertes',
+            data: [80, 100, 109, 112, 127, 131],
+            backgroundColor: 'rgba(255, 99, 132, 0.2)',
+            borderColor: 'rgba(255, 99, 132, 1)',
+            borderWidth: 1
+        },{
+            label: 'Recuperados',
+            data: [197, 214, 270, 319, 354, 452],
+            backgroundColor: 'rgba(63, 191, 70, 0.2)',
+            borderColor: 'rgba(63, 191, 70, 1)',
+            borderWidth: 1
+        }
+    ]
+    },
+    options: {
+        responsive: true,
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+};
+
+const chartElement = document.getElementById('myChart').getContext('2d');
+var myChart = new Chart(chartElement, {
+    type: 'line',
+    data: chartData.data,
+    options: chartData.options
+});
